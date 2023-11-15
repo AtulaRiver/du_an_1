1c718fdb78595dca04b739993567dd5fdbb8ad97
Toan Khanh <toanbkph31686@fpt.edu.vn>
2023-11-15 18:44:04 +0700

    dichvu php

---
+++ admin/dichvu/add.php
@@ -0,0 +1,52 @@
+<!-- Header Banner -->
+<div class="banner-header section-padding valign bg-img bg-fixed" data-overlay-dark="4"
+  data-background="../img/slider/3.jpg">
+  <div class="container">
+    <div class="row">
+      <h1>ADMIN</h1>
+      <h5>Quản lí dịch VỤ</h5>
+    </div>
+  </div>
+</div>
+
+<div class="container my-5">
+  <form action="index.php?act=adddv" method="POST">
+    <div class="space-y-12">
+      <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div class="sm:col-span-full">
+          <label for="" class="block text-sm font-medium leading-6 text-gray-900">Tên dịch vụ:</label>
+          <input style="padding-left: 12px;" type="text" name="name" id=""
+            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Nhập tên loại phòng...">
+        </div>
+      </div>
+    </div>
+
+    <div class="border-b border-gray-900/10 pb-12">
+      <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div class="sm:col-span-full">
+          <label for="" class="block text-sm font-medium leading-6 text-gray-900">Mô tả:</label>
+          <div class="mt-2">
+            <input style="padding-left: 12px;" type="text" name="mota" id=""
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Nhập giá...">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-6 flex items-center justify-end gap-x-6">
+      <button type="reset" class="text-sm font-semibold leading-6 text-gray-900">Nhập lại</button>
+      <a href="index.php?act=listsp"><input class="text-sm font-semibold leading-6 text-gray-900" type="button"
+          value="Danh sách"></a>
+      <input type="submit"
+        style="background-color: #aa8453; text-transform: none; font-family: inherit; letter-spacing: 0;" name="themdv"
+        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"></input>
+    </div>
+    <?php
+    if (isset($thongbao) && ($thongbao != "")) {
+      echo $thongbao;
+    }
+    ?>
+  </form>
+</div>
