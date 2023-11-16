5520ecf75ea3b7bea612972ffe0e67f278ebbde7
Toan Khanh <toanbkph31686@fpt.edu.vn>
2023-11-16 19:18:15 +0700

    toan: tai khoan

--- model/taikhoan.php
+++ model/taikhoan.php
@@ -3,4 +3,7 @@ function insert_taikhoan($email,$user,$pass){
     $sql="insert into taikhoan(email,user,pass) values ('$email','$user','$pass')";
     pdo_execute($sql);  
 }
+function checkuser($user,$pass){
+    $sql=" select * from taikhoan where user ='".$user."' AND pass = '".$pass."'"; 
+}
 ?>
